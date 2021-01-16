eee962ed0dcb92b8391a974921ba4e7728b0cb22
mistlog <mistlog@outlook.com>
2021-01-16 09:18:28 +0800

    fix build error in node10

--- src/translator/built-in/or.ts
+++ src/translator/built-in/or.ts
@@ -4,7 +4,8 @@ import { translateChildren } from "../common/pattern";
 
 export class OrTranslator extends Translator {
     translate(chunk: Chunk, actions: Map<string, Function>) {
-        chunk.children = chunk.children.flat();
+        // TODO: use Array.flat
+        chunk.children = [].concat(...chunk.children);
         const expression = translateChildren(chunk, actions).join(" / ");
         const pattern = `(${expression})`;
         return pattern;
